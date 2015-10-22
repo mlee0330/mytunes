c2a31d430284586c81c93c159d5dea671c8d5aa9
mlee0330 <michaelyurilee@gmail.com>
2015-10-22 16:19:11 -0700

    Passed all tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,29 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', this.firstChecker, this);
+    this.on('ended dequeue', this.dequeue, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  firstChecker: function() {
+    if (this.length === 1) {
+      this.playFirst();
+    } 
+  },
+
+  removeSong: function() {
+    this.remove(this.at(0));
+    if (this.at(0)) {
+      this.playFirst();
+    }
+  },
+
+  dequeue: function() {
+    this.removeSong();
   }
 
 });
